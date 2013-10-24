eaccee84ec5350df541713e2f3cd33a5a8d6f1e7
Pravendra Singh <hackpravj@gmail.com>
2013-10-24 16:24:56 +0530

    added 'recent' feature

--- public/js/queue.js
+++ public/js/queue.js
@@ -31,7 +31,7 @@ if(window.location.pathname == "/queue"){
       
     })
     $.get("/current",function(data){
-      $('.data').append('<div id="nowplaying" class="span8"><h2>Now Playing</h2><div class="lyrics"></div></div>');
+      $('.data').append('<div id="nowplaying" class="span4"><h2>Now Playing</h2><div class="lyrics"></div></div>');
       htmlnew='';
       if(data){
         $.get(config.muzi_root+'ajax/track/',{id:data[0]},function(track){
@@ -51,6 +51,42 @@ if(window.location.pathname == "/queue"){
         })
       }
     })
+
+    // showing recent songs
+    $.get("/recent", function(data){
+      $('#recent').remove();
+      $('.data').append('<div id="recent" class="span4"><h2>Recent</h2><ol></ol></div>');
+
+      html_recent = '';
+
+      function load_recent(x,y)
+      {
+        $.get(config.muzi_root + 'ajax/track/', {id:Number(data[x])}, function(track){
+          console.log(track);
+          html_recent += '<li mid="'+track.id
+            +'"><img style="float:left" class="thumbnail" width="50" height="50" src="'
+            +config.pics_root
+            +track.albumId
+            +'.jpg"><div class="entry1">'
+            +track.title
+            +'</div><div class="entry2">'
+            +track.artist
+            +'</div><div style="clear:both">'
+            +'</div></li>';
+          $('#recent ol').html(html_recent);
+
+          if(x+1<=y)
+          {
+            load_recent(x+1,y);
+          }
+        })
+      }
+
+      load_recent(0,(data.length - 1));
+    })
+    //
+
   })
 
 }
+
