f27604f97f994b969e9269ceac60cb3393fad6b0
Ketan Gupta <ketan19972010@gmail.com>
2016-10-04 19:59:35 +0530

    Fixes mute in Queue

--- public/js/queue.js
+++ public/js/queue.js
@@ -35,6 +35,7 @@
     QP.initialize = function(){
       if(this.checkLocation()){
         this.killHandler();
+        this.muteVolume();
         this.getNowPlaying();
         this.getQueue();
         this.getRecent();
@@ -63,6 +64,13 @@
         $.get("/kill", function() {
           location.reload();
         }).fail( This.to403 );
+      });
+    };
+
+    QP.muteVolume = function(){
+      var This = this;
+      $('.mute').click(function(){
+        $.get("/volume/mute").fail( This.to403 );
       });
     };
 
