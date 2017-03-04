3da0b950d0c7b7e655f721047c8255ea4b8b00e0
Kartikey Pandey <k.p.2511998@gmail.com>
2017-03-04 19:19:45 +0530

    Added slider in linux

--- routes/index.js
+++ routes/index.js
@@ -12,14 +12,14 @@ exports.current = function(req,res){
   res.send(vlc.getCurrent());
 };
 
-exports.youtube=function(req,res){
-  var link=req.body.link;
-  console.log(link);
-  var callback = function(data) {
-    console.log(res.send(200,data));
-  };
-  
-  vlc.play(link,'youtube', callback);
+exports.youtube = function(req, res) {
+    var link = req.body.link;
+    console.log(link);
+    var callback = function(data) {
+        console.log(res.send(200, data));
+    };
+
+    vlc.play(link, 'youtube', callback);
 };
 
 exports.youtubeInfo = function(req, res){
@@ -75,25 +75,22 @@ exports.now = function(req,res){
 }
 
 exports.volume = function(req, res) {
-  var type = req.params.type;
-  if(type == 'up') {
-    vlc.volume('+10');
-  }
-  else if(type == 'down') {
-    vlc.volume('-10');
-  }
-  else if(type == 'mute') {
-    vlc.volume('0');
-  }
-  else if(type.substring(0,4) == 'set=') {
-    console.log(type.substring(4,type.length));
-    vlc.volume(type.substring(4,type.length));  
-  }
-  else{
-    console.log("here");
-  }
-  res.send('ok');
-}
-//exports.pause=function(req,res){
-//  res.send(vlc.pause());
-//}
+        var type = req.params.type;
+        if (type == 'up') {
+            vlc.volume('+10');
+        } else if (type == 'down') {
+            vlc.volume('-10');
+        } else if (type == 'mute') {
+            vlc.volume('0');
+        } else if (type.substring(0, 4) == 'set=') {
+            console.log(type.substring(4, type.length));
+            vlc.volume(type.substring(4, type.length));
+        } else {
+            console.log("here");
+        }
+        res.send('ok');
+    }
+    //exports.pause=function(req,res){
+    //  res.send(vlc.pause());
+    //}
+
